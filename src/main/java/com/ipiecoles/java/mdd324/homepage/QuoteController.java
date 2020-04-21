ab012f9f37de418aa2085488b774b38b2d4c7d61
Aurelien <aurelien.estival@ipilyon.net>
2020-04-21 15:58:36 +0200

    commit 2 - Update QuoteControleur

--- src/main/java/com/ipiecoles/java/mdd324/homepage/QuoteController.java
+++ src/main/java/com/ipiecoles/java/mdd324/homepage/QuoteController.java
@@ -1,4 +1,17 @@
 package com.ipiecoles.java.mdd324.homepage;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/quote")
+
 public class QuoteController {
+    @Autowired
+    private Sortie sortie;
+
+    public String getQuote() {
+        return sortie.getQuote() + " " + sortie.getAuthor();
+    }
 }
