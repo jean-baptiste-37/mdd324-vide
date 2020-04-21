9dee26b927121b8fcfd3ee34fd2e1e06008c7234
Aurelien <aurelien.estival@ipilyon.net>
2020-04-21 16:26:00 +0200

    commit 2 - Update QuoteControleur final

--- src/main/java/com/ipiecoles/java/mdd324/homepage/QuoteController.java
+++ src/main/java/com/ipiecoles/java/mdd324/homepage/QuoteController.java
@@ -1,18 +1,27 @@
 package com.ipiecoles.java.mdd324.homepage;
 
+import com.ipiecoles.java.mdd324.homepage.utils.Utils;
+import com.owlike.genson.Genson;
+import com.owlike.genson.GensonBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/quote")
 
 public class QuoteController {
-    @Autowired
-    private Sortie sortie;
+    @GetMapping
+    public Sortie getQuote() throws IOException {
+        Genson genson = new GensonBuilder().useRuntimeType(true).create();
+        final String pageContents = Utils.getPageContents("http://quotes.rest/qod.json");
+        Quotes quotes = genson.deserialize(pageContents, Quotes.class);
+        return quotes.getContents().getQuotes().get(0);
 
-    public String getQuote() {
-        return sortie.getQuote() + " " + sortie.getAuthor();
     }
 }
-}
+
+       // {autho"elon Musk" "Persistance is....up." }
